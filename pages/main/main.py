0f9a87f5f9e610330957cb006ed98faab41d5b1e
Akhmetov Timur <superpracion2@gmai.com>
2024-10-17 14:03:00 +0300

    Дополнены проверки описания кнопок на главное странице

--- pages/main/main.py
+++ pages/main/main.py
@@ -12,70 +12,72 @@ class Main(Driver):
     def get_header_button(self):
         return self.driver.find_element(By.XPATH, '//*[@href="https://demoqa.com"]')
 
-
     def header_button_click(self):
         self.get_header_button().click()
         assert self.driver.current_url == 'https://demoqa.com/'
 
-
     def get_banner(self):
         return self.driver.find_element(By.XPATH, '//*[@class="home-banner"]')
-
 
     def banner_click(self):
         self.get_banner().click()
         assert self.driver.current_url == 'https://www.toolsqa.com/selenium-training/'
 
-
     def get_elements_button(self):
-        return self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][1]')
-
+        button = self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][1]')
+        assert button.text == 'Elements'
+        return button
 
     def elements_button_click(self):
         self.get_elements_button().click()
         assert self.driver.current_url == 'https://demoqa.com/elements'
 
-
     def get_forms_button(self):
-        return self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][2]')
-
+        button = self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][2]')
+        assert button.text == 'Forms'
+        return button
 
     def forms_buttons_click(self):
         self.get_forms_button().click()
         assert self.driver.current_url == 'https://demoqa.com/forms'
 
-
     def get_alerts_windows_button(self):
-        return self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][3]')
-
+        button = self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][3]')
+        assert button.text == 'Alerts, Frame & Windows'
+        return button
 
     def alerts_windows_button_click(self):
         self.get_alerts_windows_button().click()
         assert self.driver.current_url == 'https://demoqa.com/alertsWindows'
 
-
     def get_widgets_button(self):
-        return self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][4]')
-
+        button = self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][4]')
+        assert button == 'Widgets'
+        return button
 
     def widgets_button_click(self):
         self.get_widgets_button().click()
         assert self.driver.current_url == 'https://demoqa.com/widgets'
 
-
     def get_interaction_button(self):
-        return self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][5]')
-
+        button = self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][5]')
+        assert button.text == 'Interactions'
+        return button
 
     def interaction_button_click(self):
         self.get_interaction_button().click()
         assert self.driver.current_url == 'https://demoqa.com/interaction'
 
+    def get_books_button(self):
+        button = self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][6]')
+        assert button.text == 'Book Store Application'
+        return button
 
-    def get_book_button(self):
-        return self.driver.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][6]')
-
-
-    def book_button_click(self):
-        self.get_book_button().click()
+    def books_button_click(self):
+        self.get_books_button().click()
         assert self.driver.current_url == 'https://demoqa.com/books'
+
+    def get_footer(self):
+        element = self.driver.find_element(By.XPATH, '//footer')
+        assert element.text == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+        return element
