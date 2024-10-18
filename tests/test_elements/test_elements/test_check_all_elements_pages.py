eef6ce9f59b935cefcecf55413c5e3789a2c9376
Akhmetov Timur <superpracion2@gmai.com>
2024-10-18 20:25:54 +0300

    Добавлены разметка тестов для страницы elements

---
+++ tests/test_elements/test_elements/test_check_all_elements_pages.py
@@ -0,0 +1,40 @@
+from base.user import User
+from pages.main.main import Main
+
+from selenium import webdriver
+
+
+def test_open_text_box_page():
+    pass
+
+
+def test_open_check_box_page():
+    pass
+
+
+def test_open_radio_button_page():
+    pass
+
+
+def test_open_web_tables_page():
+    pass
+
+
+def test_open_buttons_page():
+    pass
+
+
+def test_open_links_page():
+    pass
+
+
+def test_open_broken_page():
+    pass
+
+
+def test_open_upload_download_page():
+    pass
+
+
+def test_open_dynamic_properties_page():
+    pass
