dca249dd711d9408c17b33af687712378bfcf76a
Akhmetov Timur <superpracion2@gmai.com>
2024-10-19 00:18:17 +0300

    изменение логики обработки текста в output поле

--- pages/elements/text_box.py
+++ pages/elements/text_box.py
@@ -1,34 +1,35 @@
 from base.driver import Driver
 from selenium.webdriver.common.by import By
 
+
 class TestBox(Driver):
     def get_full_name_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="userName"]')
 
     def input_full_name(self, value):
         self.get_full_name_field().send_keys(value)
-        assert self.get_full_name_field().text == value
+        assert self.get_full_name_field().get_attribute('value') == value
 
     def get_email_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="userEmail"]')
 
     def input_email(self, value):
         self.get_email_field().send_keys(value)
-        assert self.get_email_field().text == value
+        assert self.get_email_field().get_attribute('value') == value
 
     def get_current_address_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="currentAddress"]')
 
     def input_current_address(self, value):
         self.get_current_address_field().send_keys(value)
-        assert self.get_current_address_field().text == value
+        assert self.get_current_address_field().get_attribute('value') == value
 
     def get_permanent_address_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="permanentAddress"]')
 
     def input_permanent_address(self, value):
         self.get_permanent_address_field().send_keys(value)
-        assert self.get_permanent_address_field().text == value
+        assert self.get_permanent_address_field().get_attribute('value') == value
 
     def get_submit_button(self):
         return self.driver.find_element(By.XPATH, '//*[@id="submit"]')
@@ -46,6 +47,6 @@ class TestBox(Driver):
         if email:
             assert output_field.find_element(By.XPATH, '//*[@id="email"]').text == f'Email:{email}'
         if current_address:
-            assert output_field.find_element(By.XPATH, '//*[@id="currentAddress"]').text == f'Current Address :{current_address}'
+            assert output_field.find_element(By.XPATH, '//p[@id="currentAddress"]').text == f'Current Address :{current_address}'
         if permanent_address:
-            assert output_field.find_element(By.XPATH, '//*[@id="permanentAddress"]').text == f'Permananet Address :{permanent_address}'
+            assert output_field.find_element(By.XPATH, '//p[@id="permanentAddress"]').text == f'Permananet Address :{permanent_address}'
