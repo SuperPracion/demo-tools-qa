ffbb6a1d90afdcbfeeea623e21b575de6e449bc5
Akhmetov Timur <superpracion2@gmai.com>
2024-10-18 12:07:54 +0300

    Добавлен класс страницы elements

---
+++ pages/elements/elements.py
@@ -0,0 +1,50 @@
+from base.driver import Driver
+
+class Elements(Driver):
+    def get_text_box_button(self):
+        pass
+
+    def text_box_button_click(self):
+        pass
+
+    def get_check_box_button(self):
+        pass
+
+    def check_box_button_click(self):
+        pass
+
+    def get_radio_button(self):
+        pass
+
+    def radio_button_click(self):
+        pass
+
+    def get_web_tables_button(self):
+        pass
+
+    def web_tables_button_click(self):
+        pass
+
+    def get_links_button(self):
+        pass
+
+    def links_button_click(self):
+        pass
+
+    def get_broken_links_images_button(self):
+        pass
+
+    def broken_links_images_button_click(self):
+        pass
+
+    def get_upload_and_download_button(self):
+        pass
+
+    def upload_and_download_button_click(self):
+        pass
+
+    def get_dynamic_properties_button(self):
+        pass
+
+    def dynamic_properties_button_click(self):
+        pass
