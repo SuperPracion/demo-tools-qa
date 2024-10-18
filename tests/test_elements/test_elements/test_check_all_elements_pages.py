8602c8271ffaf55d3ac07030e9d32e99e1a1e712
Akhmetov Timur <superpracion2@gmai.com>
2024-10-18 21:56:34 +0300

    добавлены тела тестов на проверку доступности страниц elements

--- tests/test_elements/test_elements/test_check_all_elements_pages.py
+++ tests/test_elements/test_elements/test_check_all_elements_pages.py
@@ -1,40 +1,63 @@
+import pytest
 from base.user import User
 from pages.main.main import Main
+from pages.elements.elements import Elements
 
 from selenium import webdriver
 
 
-def test_open_text_box_page():
-    pass
+@pytest.fixture
+def setup_user():
+    user = User()
+    yield user
 
 
-def test_open_check_box_page():
-    pass
+@pytest.fixture
+def setup_drive():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.close()
 
 
-def test_open_radio_button_page():
-    pass
+@pytest.fixture
+def elements(setup_user, setup_drive):
+    main_page = Main(setup_user, setup_drive)
+    main_page.elements_button_click()
+    elements = Elements(setup_drive)
+    yield elements
 
 
-def test_open_web_tables_page():
-    pass
+def test_open_text_box_page(elements):
+    elements.text_box_button_click()
 
 
-def test_open_buttons_page():
-    pass
+def test_open_check_box_page(elements):
+    elements.check_box_button_click()
 
 
-def test_open_links_page():
-    pass
+def test_open_radio_button_page(elements):
+    elements.radio_button_click()
 
 
-def test_open_broken_page():
-    pass
+def test_open_web_tables_page(elements):
+    elements.web_tables_button_click()
 
 
-def test_open_upload_download_page():
-    pass
+def test_open_buttons_page(elements):
+    elements.buttons_button_click()
 
 
-def test_open_dynamic_properties_page():
-    pass
+def test_open_links_page(elements):
+    elements.links_button_click()
+
+
+def test_open_broken_page(elements):
+    elements.broken_links_images_button_click()
+
+
+def test_open_upload_download_page(elements):
+    elements.upload_and_download_button_click()
+
+
+def test_open_dynamic_properties_page(elements):
+    elements.dynamic_properties_button_click()
