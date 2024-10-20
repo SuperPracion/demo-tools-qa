b102bab6f3f65a9a2a7aee32ec6f238c5f4411ac
Akhmetov Timur <superpracion2@gmai.com>
2024-10-20 09:09:28 +0300

    Добавлены тесты для browser windows страницы

--- pages/alerts_windows/browser_windows.py
+++ pages/alerts_windows/browser_windows.py
@@ -10,18 +10,38 @@ class BrowserWindows(Driver):
 
     def new_tab_button_click(self):
         self.get_new_tab_button().click()
-        pass
+
+        original_window = self.driver.current_window_handle
+        assert len(self.driver.window_handles) > 1
+
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        assert self.driver.find_element(By.XPATH, "//*[@id='sampleHeading']").text == 'This is a sample page'
+
+        self.driver.switch_to.window(original_window)
 
     def get_new_window_button(self):
         return self.driver.find_element(By.XPATH, "//*[@id='tabButton']")
 
     def new_window_button_click(self):
         self.get_new_window_button().click()
-        pass
+
+        original_window = self.driver.current_window_handle
+        assert len(self.driver.window_handles) > 1
+
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        assert self.driver.find_element(By.XPATH, "//*[@id='sampleHeading']").text == 'This is a sample page'
+
+        self.driver.switch_to.window(original_window)
+
 
     def get_new_window_message_button(self):
         return self.driver.find_element(By.XPATH, "//*[@id='messageWindowButton']")
 
     def new_window_message_button_click(self):
         self.get_new_window_message_button().click()
-        pass
+
+        original_window = self.driver.current_window_handle
+        assert len(self.driver.window_handles) > 1
+
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.switch_to.window(original_window)
