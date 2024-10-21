7fb846bcee9714dfe9fede492c62bfcb85e4ee6d
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-21 14:10:56 +0300

    Исправлена работа с iframes на странице Frames
    Signed-off-by: unknown <superpracion2@gmail.com>

--- pages/alerts_windows/frames.py
+++ pages/alerts_windows/frames.py
@@ -3,19 +3,19 @@ from selenium.webdriver.common.by import By
 from base.driver import Driver
 
 
-class Framer(Driver):
+class Frames(Driver):
     def get_frame1(self):
-        return self.driver.find_element(By.XPATH, '//*[@id="frame1Wrapper"]')
+        return self.driver.find_element(By.XPATH, "//iframe[@id='frame1']")
 
     def switch_to_frame1(self):
         frame = self.get_frame1()
         self.driver.switch_to.frame(frame)
-        assert self.driver.find_element(By.XPATH, '//*[@id="sampleHeading"]') == 'This is a sample page'
+        assert self.driver.find_element(By.XPATH, '//*[@id="sampleHeading"]').text == 'This is a sample page'
 
     def get_frame2(self):
-        return self.driver.find_element(By.XPATH, '//*[@id="frame2Wrapper"]')
+        return self.driver.find_element(By.XPATH, "//iframe[@id='frame2']")
 
     def switch_to_frame2(self):
         frame = self.get_frame2()
         self.driver.switch_to.frame(frame)
-        assert self.driver.find_element(By.XPATH, '//*[@id="sampleHeading"]') == 'This is a sample page'
+        assert self.driver.find_element(By.XPATH, '//*[@id="sampleHeading"]').text == 'This is a sample page'
