41482aad2a309f78337b1435a208df15ca58a202
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-21 13:11:40 +0300

    Добавлены методы для работы с вводимым значением в поле alert
    Signed-off-by: unknown <superpracion2@gmail.com>

--- pages/alerts_windows/alerts.py
+++ pages/alerts_windows/alerts.py
@@ -1,5 +1,5 @@
-from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 from base.driver import Driver
 
@@ -10,26 +10,50 @@ class Alerts(Driver):
 
     def alert_button_click(self):
         self.get_alert_button().click()
-        return self.driver.switch_to.alert
+
+    def alert_accept(self):
+        alert = self.driver.switch_to.alert
+        alert.accept()
+
+    def alert_dismiss(self):
+        alert = self.driver.switch_to.alert
+        alert.dismiss()
 
     def get_timer_alert_button(self):
         return self.driver.find_element(By.XPATH, '//*[@id="timerAlertButton"]')
 
     def time_alert_button_click(self):
         self.get_timer_alert_button().click()
-        sleep(6)
-        return self.driver.switch_to.alert
 
     def get_confirm_alert_button(self):
         return self.driver.find_element(By.XPATH, '//*[@id="confirmButton"]')
 
     def confirm_alert_button_click(self):
         self.get_confirm_alert_button().click()
-        return self.driver.switch_to.alert
+
+    def confirm_alert_accept(self):
+        self.alert_accept()
+        assert self.driver.find_element(By.XPATH, '//*[@id="confirmResult"]').text =='You selected Ok'
+
+    def confirm_alert_dismiss(self):
+        self.alert_dismiss()
+        assert self.driver.find_element(By.XPATH, '//*[@id="confirmResult"]').text == 'You selected Cancel'
 
     def get_promt_alert_button(self):
         return self.driver.find_element(By.XPATH, '//*[@id="promtButton"]')
 
     def promt_alert_button_click(self):
         self.get_promt_alert_button().click()
-        return self.driver.switch_to.alert
+
+    def promt_alert_accept(self, value):
+        alert = self.driver.switch_to.alert
+        alert.send_keys(value)
+        self.alert_accept()
+        assert self.driver.find_element(By.XPATH, '//*[@id="promptResult"]').text == f'You entered {value}'
+
+    def promt_alert_dismiss(self):
+        self.alert_dismiss()
+        try:
+            self.driver.find_element(By.XPATH, '//*[@id="promptResult"]')
+        except NoSuchElementException:
+            pass
