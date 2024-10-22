6909b8f19830e460c61094031eadbb58ad3f284e
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-22 08:12:14 +0300

    Расширен класс nestedframes для переключения на дочерний iframe, изменён поиск родительского iframe
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/alerts_windows/nestedframes.py
+++ pages/alerts_windows/nestedframes.py
@@ -1,10 +1,11 @@
-from base.driver import Driver
 from selenium.webdriver.common.by import By
+
+from base.driver import Driver
 
 
 class NestedFrames(Driver):
     def get_parent_frame(self):
-        return self.driver.find_element(By.XPATH, '//*[@id="frame1Wrapper"]')
+        return self.driver.find_element(By.XPATH, '//*[@id="frame1"]')
 
     def switch_to_parent_frame(self):
         frame = self.get_parent_frame()
@@ -15,6 +16,7 @@ class NestedFrames(Driver):
         return self.driver.find_element(By.XPATH, '//*[@srcdoc="<p>Child Iframe</p>"]')
 
     def switch_to_child_frame(self):
+        self.switch_to_parent_frame()
         frame = self.get_child_frame()
         self.driver.switch_to.frame(frame)
         assert self.driver.find_element(By.XPATH, '//body').text == 'Child Iframe'
