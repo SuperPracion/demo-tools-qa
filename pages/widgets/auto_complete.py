4137b63a786951b3b2aa4cccdc902f9569470909
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-23 11:43:17 +0300

    Расширены проверки на ресурсе AutoComplete
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/widgets/auto_complete.py
+++ pages/widgets/auto_complete.py
@@ -1,3 +1,4 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -6,17 +7,29 @@ from base.driver import Driver
 
 class AutoComplete(Driver):
     def get_auto_complete_multiple_field(self):
-        return self.driver.find_element(By.XPATH, '//*[@id="autoCompleteMultipleContainer"]')
+        return self.driver.find_element(By.XPATH, '//*[@id="autoCompleteMultipleInput"]')
 
     def auto_complete_multiple_write(self, value):
-        field = self.get_auto_complete_single_field()
+        field = self.get_auto_complete_multiple_field()
+        field.click()
         field.send_keys(value)
         field.send_keys(Keys.ENTER)
+        for i in range(10):
+            try:
+                if value in self.driver.find_element(By.XPATH, f'(//*[@class="css-12jo7m5 auto-complete__multi-value__label"])[{i}]').text:
+                    break
+            except:
+                pass
+        else:
+            raise NoSuchElementException
 
     def get_auto_complete_single_field(self):
-        return self.driver.find_element(By.XPATH, '//*[@id="autoCompleteSingleContainer"]')
+        return self.driver.find_element(By.XPATH, '//*[@id="autoCompleteSingleInput"]')
 
     def auto_complete_single_write(self, value):
-        field = self.get_auto_complete_multiple_field()
+        field = self.get_auto_complete_single_field()
+        field.click()
         field.send_keys(value)
         field.send_keys(Keys.ENTER)
+        field.send_keys(Keys.ENTER)
+        assert value in self.driver.find_element(By.XPATH, '//*[@class="auto-complete__single-value css-1uccc91-singleValue"]').text
