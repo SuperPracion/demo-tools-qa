2f7aa0573357a17b4c973ffb55cff06608b1993a
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-23 10:24:42 +0300

    Добавлены тесты для ресурса Accordian
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

---
+++ tests_pages/tests_widgets/test_accordian/test_open_accordians_cards.py
@@ -0,0 +1,27 @@
+import pytest
+
+from pages.main.main import Main
+from pages.widgets.widgets import Widgets
+from pages.widgets.accordian import Accordian
+
+
+@pytest.fixture
+def accordian(setup_user, setup_driver):
+    main_page = Main(setup_user, setup_driver)
+    main_page.widgets_button_click()
+    widgets = Widgets(setup_driver)
+    widgets.accordian_button_click()
+    accordian = Accordian(setup_driver)
+    yield accordian
+
+
+def test_what_is_lorem_open(accordian):
+    accordian.card_header_what_is_lorem_click()
+
+
+def test_where_does_it_come_from_open(accordian):
+    accordian.card_header_where_does_it_come_from_click()
+
+
+def test_why_do_we_use_it(accordian):
+    accordian.card_header_why_do_we_use_it_click()
