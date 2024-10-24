4010d7237c5339c185c1f3cae65c5e6a06c43567
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-24 10:14:52 +0300

    Описаны тесты для ресурса Slider
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

---
+++ tests_pages/test_widgets/test_slider/test_move_slider.py
@@ -0,0 +1,26 @@
+import pytest
+
+from pages.main.main import Main
+from pages.widgets.widgets import Widgets
+from pages.widgets.slider import Slider
+from tests_pages.conftest import setup_driver
+
+
+@pytest.fixture
+def slider(setup_user, setup_driver):
+    main_page = Main(setup_user, setup_driver)
+    main_page.widgets_button_click()
+    widgets = Widgets(setup_driver)
+    widgets.slider_button_click()
+    slider = Slider(setup_driver)
+    yield slider
+
+
+def test_set_value_to_field(slider):
+    slider.set_slider_field_value(-80)
+    assert slider.get_range_slider().get_attribute('value') == '25'
+
+
+def test_move_slider(slider):
+    slider.set_range_slider_value(80)
+    assert slider.get_slider_field().get_attribute('value') == '69'
