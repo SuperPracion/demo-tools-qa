5dbc7b7bc1922221874decb6f004b0f6a19682f1
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-25 14:55:51 +0300

    Добавлены allure декораторы для фиксации процесс на ресурсе Tabs
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests_pages/test_widgets/test_tabs/test_tabbing.py
+++ tests_pages/test_widgets/test_tabs/test_tabbing.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 
 from pages.main.main import Main
@@ -6,6 +7,7 @@ from pages.widgets.tabs import Tabs
 
 
 @pytest.fixture
+@allure.title('Tabs fixture')
 def tabs(setup_user, setup_driver):
     main_page = Main(setup_user, setup_driver)
     main_page.widgets_button_click()
@@ -14,17 +16,22 @@ def tabs(setup_user, setup_driver):
     tabs = Tabs(setup_driver)
     yield tabs
 
+
+@allure.title('Open tab what ')
 def test_open_what_tab(tabs):
     tabs.what_tab_click()
 
 
+@allure.title('Open tab Origin')
 def test_open_origin_tab(tabs):
     tabs.origin_tab_click()
 
 
+@allure.title('Open tab Use')
 def test_open_use_tab(tabs):
     tabs.use_tab_click()
 
 
+@allure.title('Open tab More')
 def test_open_more_tab(tabs):
     tabs.more_tab_click()
