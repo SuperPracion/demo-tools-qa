df1e1d776bd2e26590c4d2ffb4bde45d7d793808
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-29 16:51:39 +0300

    fixture вынесена в conf для формирования древовидной структуры ресурсов
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_elements/test_elements/test_opening_elements_pages.py
+++ tests/test_elements/test_elements/test_opening_elements_pages.py
@@ -4,45 +4,37 @@ from pages.main.main import Main
 from pages.elements.elements import Elements
 
 
-@pytest.fixture
-def elements(setup_user, setup_driver):
-    main_page = Main(setup_user, setup_driver)
-    main_page.elements_button_click()
-    elements = Elements(setup_driver)
-    yield elements
+def test_open_text_box_page(elements_page):
+    elements_page.text_box_button_click()
 
 
-def test_open_text_box_page(elements):
-    elements.text_box_button_click()
+def test_open_check_box_page(elements_page):
+    elements_page.checkbox_button_click()
 
 
-def test_open_check_box_page(elements):
-    elements.checkbox_button_click()
+def test_open_radio_button_page(elements_page):
+    elements_page.radio_button_click()
 
 
-def test_open_radio_button_page(elements):
-    elements.radio_button_click()
+def test_open_web_tables_page(elements_page):
+    elements_page.web_tables_button_click()
 
 
-def test_open_web_tables_page(elements):
-    elements.web_tables_button_click()
+def test_open_buttons_page(elements_page):
+    elements_page.buttons_button_click()
 
 
-def test_open_buttons_page(elements):
-    elements.buttons_button_click()
+def test_open_links_page(elements_page):
+    elements_page.links_button_click()
 
 
-def test_open_links_page(elements):
-    elements.links_button_click()
+def test_open_broken_page(elements_page):
+    elements_page.broken_links_images_button_click()
 
 
-def test_open_broken_page(elements):
-    elements.broken_links_images_button_click()
+def test_open_upload_download_page(elements_page):
+    elements_page.upload_and_download_button_click()
 
 
-def test_open_upload_download_page(elements):
-    elements.upload_and_download_button_click()
-
-
-def test_open_dynamic_properties_page(elements):
-    elements.dynamic_properties_button_click()
+def test_open_dynamic_properties_page(elements_page):
+    elements_page.dynamic_properties_button_click()
