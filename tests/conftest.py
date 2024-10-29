a991b78435aa0c29acb3b4eef2ec99447a3d5d74
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-29 16:52:19 +0300

    Добавлена общая fixture для ресурса alert
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 # test/conftest.py
 import pytest
+import allure
 from selenium import webdriver
 
 from base.user import User
 from pages.main.main import Main
+from pages.elements.elements import Elements
 
 
 @pytest.fixture
@@ -26,3 +28,11 @@ def setup_driver():
 def main_page(setup_user, setup_driver):
     main_page = Main(setup_user, setup_driver)
     yield main_page
+
+
+@pytest.fixture
+@allure.step('open elements page')
+def elements_page(main_page):
+    main_page.click(main_page.get_elements_button())
+    elements_page = Elements(main_page.driver)
+    yield elements_page
