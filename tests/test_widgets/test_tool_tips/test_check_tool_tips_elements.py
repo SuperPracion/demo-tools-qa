0e5d2805bf5fe024935ec07a449e4173a13b1c84
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:37:59 +0300

    Переписаны тесты для ресурса ToolTips
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_widgets/test_tool_tips/test_check_tool_tips_elements.py
+++ tests/test_widgets/test_tool_tips/test_check_tool_tips_elements.py
@@ -1,37 +1,28 @@
-import pytest
+import allure
 from selenium.webdriver.common.by import By
 
-from pages.main.main import Main
-from pages.widgets.widgets import Widgets
-from pages.widgets.tool_tips import ToolTips
+
+@allure.title('')
+class TestToolTips:
+    @allure.step('')
+    def test_button_tool_tips(self, tool_tips_page):
+        tip_button = tool_tips_page.get_tools_tip_button()
+        tool_tips_page.hover_over_element(tip_button)
+        assert 'You hovered over the' in tool_tips_page.driver.find_element(By.XPATH, '//*[@class="tooltip-inner"]').text
+
+    @allure.step('')
+    def test_field_tool_tips(self, tool_tips_page):
+        tip_field = tool_tips_page.get_tools_tip_field()
+        tool_tips_page.hover_over_element(tip_field)
+        assert 'You hovered over the' in tool_tips_page.driver.find_element(By.XPATH, '//*[@class="tooltip-inner"]').text
 
 
-@pytest.fixture
-def tool_tips(setup_user, setup_driver):
-    main_page = Main(setup_user, setup_driver)
-    main_page.widgets_button_click()
-    widgets = Widgets(setup_driver)
-    widgets.tool_tips_button_click()
-    tool_tips = ToolTips(setup_driver)
-    yield tool_tips
+    @allure.step('')
+    def test_container_tool_tips(self, tool_tips_page):
+        element = tool_tips_page.get_tools_tip_container().find_element(By.XPATH, '//*[contains(text(), "Contrary")]')
+        tool_tips_page.hover_over_element(element)
+        assert 'You hovered over the' in tool_tips_page.driver.find_element(By.XPATH, '//*[@class="tooltip-inner"]').text
 
-
-def tool_tip_check(tool_tips):
-    assert 'You hovered over the' in tool_tips.driver.find_element(By.XPATH, '//*[@class="tooltip-inner"]').text
-
-
-def test_button_tool_tips(tool_tips):
-    tool_tips.move_to_tool_tip_button()
-    tool_tip_check(tool_tips)
-
-
-def test_field_tool_tips(tool_tips):
-    tool_tips.move_to_tools_tip_field()
-    tool_tip_check(tool_tips)
-
-
-def test_container_tool_tips(tool_tips):
-    tool_tips.move_to_tools_tip_container_contrary()
-    tool_tip_check(tool_tips)
-    tool_tips.move_to_tools_tip_container_1_10_32()
-    tool_tip_check(tool_tips)
+        element = tool_tips_page.get_tools_tip_container().find_element(By.XPATH, '//*[contains(text(), "1.10.32")]')
+        tool_tips_page.hover_over_element(element)
+        assert 'You hovered over the' in tool_tips_page.driver.find_element(By.XPATH, '//*[@class="tooltip-inner"]').text
