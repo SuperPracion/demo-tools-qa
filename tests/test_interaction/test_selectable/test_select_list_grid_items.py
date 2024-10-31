6aebf0210642db3f277b9c6114b52e163dbdb2e0
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:34:21 +0300

    Переписаны тесты для ресурса Selectable
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_interaction/test_selectable/test_select_list_grid_items.py
+++ tests/test_interaction/test_selectable/test_select_list_grid_items.py
@@ -1,31 +1,25 @@
-import pytest
+import allure
 from selenium.webdriver.common.by import By
 
-from pages.main.main import Main
-from pages.interaction.interaction import Interaction
-from pages.interaction.selectable import Selectable
 
+@allure.title('')
+class TestSelectableItems:
+    @allure.step('')
+    def test_select_item_in_list(self, selectable_page):
+        list_button = selectable_page.get_demo_tab_list()
+        selectable_page.click(list_button)
+        item = selectable_page.get_group_item_by_text('Dapibus ac facilisis in')
+        selectable_page.click(item)
+        assert 'active' in item.get_attribute('class')
+        assert len(selectable_page.driver.find_elements(By.XPATH,
+                                                        '//*[@class="mt-2 list-group-item list-group-item-action"]')) == 3
 
-@pytest.fixture
-def selectable(setup_user, setup_driver):
-    main_page = Main(setup_user, setup_driver)
-    main_page.interaction_button_click()
-    interaction = Interaction(setup_driver)
-    interaction.selectable_button_click()
-    selectable = Selectable(setup_driver)
-    yield selectable
-
-
-def test_select_item_in_list(selectable):
-    selectable.demo_tab_list_click()
-    item = selectable.get_group_item_by_text('Dapibus ac facilisis in')
-    item.click()
-    assert 'active' in item.get_attribute('class')
-    assert len(selectable.driver.find_elements(By.XPATH, '//*[@class="mt-2 list-group-item list-group-item-action"]')) == 3
-
-def test_select_item_in_grid(selectable):
-    selectable.demo_tab_grid_click()
-    item = selectable.get_group_item_by_text('Five')
-    item.click()
-    assert 'active' in item.get_attribute('class')
-    assert len(selectable.driver.find_elements(By.XPATH, '//*[@class="list-group-item list-group-item-action"]')) == 8
+    @allure.step('')
+    def test_select_item_in_grid(self, selectable_page):
+        grid_button = selectable_page.get_demo_tab_grid()
+        selectable_page.click(grid_button)
+        item = selectable_page.get_group_item_by_text('Five')
+        selectable_page.click(item)
+        assert 'active' in item.get_attribute('class')
+        assert len(
+            selectable_page.driver.find_elements(By.XPATH, '//*[@class="list-group-item list-group-item-action"]')) == 8
