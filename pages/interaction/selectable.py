a234328ecb58b9a0b7aea3fd894afcc3cc2fff5b
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:39:13 +0300

    Убраны методы для самопроверки в классе описывающим ресурс Selectable
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/interaction/selectable.py
+++ pages/interaction/selectable.py
@@ -7,14 +7,8 @@ class Selectable(Driver):
     def get_demo_tab_list(self):
         return self.driver.find_element(By.XPATH, '//*[@id="demo-tab-list"]')
 
-    def demo_tab_list_click(self):
-        self.get_demo_tab_list().click()
-
     def get_demo_tab_grid(self):
         return self.driver.find_element(By.XPATH, '//*[@id="demo-tab-grid"]')
-
-    def demo_tab_grid_click(self):
-        self.get_demo_tab_grid().click()
 
     def get_group_item_by_text(self, text):
         return self.driver.find_element(By.XPATH, f'//*[contains(text(), "{text}")]')
