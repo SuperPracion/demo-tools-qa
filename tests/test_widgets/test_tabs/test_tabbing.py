3d00cb9c13a0f451551d92b3ff1b7baed7557afc
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:33:11 +0300

    Переписаны тесты для ресурса Tabs
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_widgets/test_tabs/test_tabbing.py
+++ tests/test_widgets/test_tabs/test_tabbing.py
@@ -1,37 +1,29 @@
 import allure
-import pytest
-
-from pages.main.main import Main
-from pages.widgets.widgets import Widgets
-from pages.widgets.tabs import Tabs
 
 
-@pytest.fixture
-@allure.title('Tabs fixture')
-def tabs(setup_user, setup_driver):
-    main_page = Main(setup_user, setup_driver)
-    main_page.widgets_button_click()
-    widgets = Widgets(setup_driver)
-    widgets.tabs_button_click()
-    tabs = Tabs(setup_driver)
-    yield tabs
+@allure.title('')
+class TestTabbing:
+    @allure.title('open tab what')
+    def test_open_what_tab(self, tabs_page):
+        tab = tabs_page.get_what_tab()
+        tabs_page.click(tab)
+        assert tab.get_attribute('aria-selected') == 'true'
 
+    @allure.title('open tab Origin')
+    def test_open_origin_tab(self, tabs_page):
+        tab = tabs_page.get_origin_tab()
+        tabs_page.click(tab)
+        assert tab.get_attribute('aria-selected') == 'true'
 
-@allure.title('Open tab what ')
-def test_open_what_tab(tabs):
-    tabs.what_tab_click()
+    @allure.title('open tab Use')
+    def test_open_use_tab(self, tabs_page):
+        tab = tabs_page.get_use_tab()
+        tabs_page.click(tab)
+        assert tab.get_attribute('aria-selected') == 'true'
 
-
-@allure.title('Open tab Origin')
-def test_open_origin_tab(tabs):
-    tabs.origin_tab_click()
-
-
-@allure.title('Open tab Use')
-def test_open_use_tab(tabs):
-    tabs.use_tab_click()
-
-
-@allure.title('Open tab More')
-def test_open_more_tab(tabs):
-    tabs.more_tab_click()
+    @allure.title('open tab More')
+    def test_open_more_tab(self, tabs_page):
+        tab = tabs_page.get_more_tab()
+        tabs_page.click(tab)
+        assert tab.is_enabled()
+        # assert self.get_more_tab().get_attribute('aria-selected') == 'true'
