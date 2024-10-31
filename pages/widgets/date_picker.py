6b731ec98dbb4f1fc0f2e6970be9fdd01fbcaad4
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:40:55 +0300

    Убраны методы для самопроверки в классе описывающим ресурс Date Picker
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/widgets/date_picker.py
+++ pages/widgets/date_picker.py
@@ -1,6 +1,5 @@
 from base.driver import Driver
 
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
@@ -8,21 +7,5 @@ class DatePicker(Driver):
     def get_date_picker_month_year_input(self):
         return self.driver.find_element(By.XPATH, '//*[@id="datePickerMonthYearInput"]')
 
-    def set_date_picker_mont_year(self, day, month='', year=''):
-        field = self.get_date_picker_month_year_input()
-        field.click()
-        field.send_keys(Keys.CONTROL + 'a')
-        field.send_keys(Keys.BACKSPACE)
-        field.send_keys('/'.join([day, month, year]))
-        field.send_keys(Keys.ENTER)
-
     def get_date_and_time_picker_input(self):
         return self.driver.find_element(By.XPATH, '//*[@id="dateAndTimePickerInput"]')
-
-    def set_date_and_time_picker(self, day, month='', year='', hour='', minute=''):
-        field = self.get_date_and_time_picker_input()
-        field.click()
-        field.send_keys(Keys.CONTROL + 'a')
-        field.send_keys(Keys.BACKSPACE)
-        field.send_keys('/'.join([day, month, year]) + ' ' + ':'.join([hour, minute]))
-        field.send_keys(Keys.ENTER)
