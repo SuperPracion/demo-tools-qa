f83b73abfde7589f562849693dcd1468ab54fb27
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:42:11 +0300

    Убраны методы для самопроверки в классе описывающим ресурс Accordion
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/widgets/accordian.py
+++ pages/widgets/accordian.py
@@ -5,22 +5,10 @@ from base.driver import Driver
 
 class Accordian(Driver):
     def get_card_header_what_is_lorem(self):
-         return self.driver.find_element(By.XPATH, '//*[@id="section1Heading"]')
-
-    def card_header_what_is_lorem_click(self):
-        self.get_card_header_what_is_lorem().click()
-        assert self.driver.find_element(By.XPATH, '//*[@id="section1Content"]').text
+        return self.driver.find_element(By.XPATH, '//*[@id="section1Heading"]')
 
     def get_card_header_where_does_it_come_from(self):
         return self.driver.find_element(By.XPATH, '//*[@id="section2Heading"]')
 
-    def card_header_where_does_it_come_from_click(self):
-        self.get_card_header_where_does_it_come_from().click()
-        assert self.driver.find_element(By.XPATH, '//*[@id="section2Content"]/p').text
-
     def get_card_header_why_do_we_use_it(self):
         return self.driver.find_element(By.XPATH, '//*[@id="section3Heading"]')
-
-    def card_header_why_do_we_use_it_click(self):
-        self.get_card_header_why_do_we_use_it().click()
-        assert self.driver.find_element(By.XPATH, '//*[@id="section3Content"]/p').text
