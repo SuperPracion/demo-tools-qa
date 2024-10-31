058440dd8957d0ef4af97adea3f6493ee971ef70
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:39:56 +0300

    Убраны методы для самопроверки в классе описывающим ресурс Interaction
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/interaction/interaction.py
+++ pages/interaction/interaction.py
@@ -8,34 +8,34 @@ class Interaction(Driver):
     def get_sortable_button(self):
         return self.driver.find_element(By.XPATH, "//*[contains(text(), 'Sortable')]/parent::*")
 
-    def sortable_button_click(self):
-        self.get_sortable_button().click()
-        assert self.driver.current_url == f'{MAIN_URL}/sortable'
+    # def sortable_button_click(self):
+    #     self.get_sortable_button().click()
+    #     assert self.driver.current_url == f'{MAIN_URL}/sortable'
 
     def get_selectable_button(self):
         return self.driver.find_element(By.XPATH, "//*[contains(text(), 'Selectable')]/parent::*")
 
-    def selectable_button_click(self):
-        self.get_selectable_button().click()
-        assert self.driver.current_url == f'{MAIN_URL}/selectable'
+    #     self.get_selectable_button().click()
+    # def selectable_button_click(self):
+    #     assert self.driver.current_url == f'{MAIN_URL}/selectable'
 
     def get_resizable_button(self):
         return self.driver.find_element(By.XPATH, "//*[contains(text(), 'Resizable')]/parent::*")
 
-    def resizable_button_click(self):
-        self.get_resizable_button().click()
-        assert self.driver.current_url == f'{MAIN_URL}/resizable'
+    # def resizable_button_click(self):
+    #     self.get_resizable_button().click()
+    #     assert self.driver.current_url == f'{MAIN_URL}/resizable'
 
     def get_droppable_button(self):
         return self.driver.find_element(By.XPATH, "//*[contains(text(), 'Droppable')]/parent::*")
 
-    def droppable_button_click(self):
-        self.get_droppable_button().click()
-        assert self.driver.current_url == f'{MAIN_URL}/droppable'
+    # def droppable_button_click(self):
+    #     self.get_droppable_button().click()
+    #     assert self.driver.current_url == f'{MAIN_URL}/droppable'
 
     def get_dragabble_button(self):
         return self.driver.find_element(By.XPATH, "//*[contains(text(), 'Dragabble')]/parent::li")
 
-    def dragabble_button_click(self):
-        self.get_dragabble_button().click()
-        assert self.driver.current_url == f'{MAIN_URL}/dragabble'
+    # def dragabble_button_click(self):
+    #     self.get_dragabble_button().click()
+    #     assert self.driver.current_url == f'{MAIN_URL}/dragabble'
