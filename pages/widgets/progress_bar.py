921523ecb494f11896d00d71ff8f2d7cf4cb424f
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:39:44 +0300

    Убраны методы для самопроверки в классе описывающим ресурс Progress Bar
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/widgets/progress_bar.py
+++ pages/widgets/progress_bar.py
@@ -2,18 +2,13 @@ from selenium.webdriver.common.by import By
 
 from base.driver import Driver
 
+
 class ProgressBar(Driver):
     def get_start_stop_button(self):
         return self.driver.find_element(By.XPATH, '//*[@id="startStopButton"]')
-
-    def start_stop_button_click(self):
-        self.get_start_stop_button().click()
 
     def get_progress_bar(self):
         return self.driver.find_element(By.XPATH, '//*[@role="progressbar"]')
 
     def get_reset_button(self):
         return self.driver.find_element(By.XPATH, '//*[@id="resetButton"]')
-
-    def rest_button_click(self):
-        self.get_reset_button().click()
