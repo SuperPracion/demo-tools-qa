ce0a16e8e964f700a00f7f82fe48a1479803cf5d
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:32:53 +0300

    Переписаны тесты для ресурса TextBox
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_elements/test_text_box/test_text_box_positive_filling.py
+++ tests/test_elements/test_text_box/test_text_box_positive_filling.py
@@ -1,57 +1,32 @@
-import pytest
-
-from pages.main.main import Main
-from pages.elements.elements import Elements
-from pages.elements.text_box import TextBox
+def test_filling_current_address(text_box_page):
+    text_box_page.input_current_address('123')
 
 
-@pytest.fixture
-def text_box(setup_user, setup_driver):
-    main_page = Main(setup_user, setup_driver)
-    main_page.elements_button_click()
-    elements = Elements(setup_driver)
-    elements.text_box_button_click()
-    text_box = TextBox(setup_driver)
-    yield text_box
+def test_filling_permanent_address(text_box_page):
+    text_box_page.input_permanent_address('123')
 
 
-def test_filling_fullname(text_box):
-    text_box.input_full_name('123')
+def test_all_fields_filling(text_box_page):
+    text_box_page.input_full_name('Иванов')
+    text_box_page.input_email('ivanov@gmail.com')
+    text_box_page.input_current_address('Moscow')
+    text_box_page.input_permanent_address('SPb')
 
 
-def test_filling_email(text_box):
-    text_box.input_email('123')
-
-
-def test_filling_current_address(text_box):
-    text_box.input_current_address('123')
-
-
-def test_filling_permanent_address(text_box):
-    text_box.input_permanent_address('123')
-
-
-def test_all_fields_filling(text_box):
-    text_box.input_full_name('Иванов')
-    text_box.input_email('ivanov@gmail.com')
-    text_box.input_current_address('Moscow')
-    text_box.input_permanent_address('SPb')
-
-
-def test_output_visualization(text_box):
+def test_output_visualization(text_box_page):
     full_name = 'Ivanov Ivan Ivanovich'
     email = 'ivanov@gmail.com'
     current_address = 'Moscow'
     permanent_address = 'SPb'
 
-    text_box.input_full_name(full_name)
-    text_box.input_email(email)
-    text_box.input_current_address(current_address)
-    text_box.input_permanent_address(permanent_address)
-    text_box.submit_button_click()
-    text_box.output_field_check_values(full_name, email, current_address, permanent_address)
+    text_box_page.input_full_name(full_name)
+    text_box_page.input_email(email)
+    text_box_page.input_current_address(current_address)
+    text_box_page.input_permanent_address(permanent_address)
+    text_box_page.submit_button_click()
+    text_box_page.output_field_check_values(full_name, email, current_address, permanent_address)
 
 
-def test_email_field_error(text_box):
-    text_box.input_email('!')
-    text_box.submit_button_click()
+def test_email_field_error(text_box_page):
+    text_box_page.input_email('!')
+    text_box_page.submit_button_click()
