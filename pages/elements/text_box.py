41d5c4b7a400e6d81d9469e04b60c09cc8e5345a
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:30:49 +0300

    Убраны методы для самопроверки в классе описывающим ресурс TextBox
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/elements/text_box.py
+++ pages/elements/text_box.py
@@ -7,53 +7,43 @@ class TextBox(Driver):
     def get_full_name_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="userName"]')
 
-    def input_full_name(self, value):
-        self.get_full_name_field().send_keys(value)
-        assert self.get_full_name_field().get_attribute('placeholder') == 'Full Name'
-        assert self.get_full_name_field().get_attribute('value') == value
-
     def get_email_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="userEmail"]')
-
-    def input_email(self, value):
-        self.get_email_field().send_keys(value)
-        assert self.get_email_field().get_attribute('placeholder') == 'name@example.com'
-        assert self.get_email_field().get_attribute('value') == value
 
     def get_current_address_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="currentAddress"]')
 
-    def input_current_address(self, value):
-        self.get_current_address_field().send_keys(value)
-        assert self.get_current_address_field().get_attribute('placeholder') == 'Current Address'
-        assert self.get_current_address_field().get_attribute('value') == value
+    # def input_current_address(self, value):
+    #     self.get_current_address_field().send_keys(value)
+    #     assert self.get_current_address_field().get_attribute('placeholder') == 'Current Address'
+    #     assert self.get_current_address_field().get_attribute('value') == value
 
     def get_permanent_address_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="permanentAddress"]')
 
-    def input_permanent_address(self, value):
-        self.get_permanent_address_field().send_keys(value)
-        assert self.get_permanent_address_field().get_attribute('value') == value
+    # def input_permanent_address(self, value):
+    #     self.get_permanent_address_field().send_keys(value)
+    #     assert self.get_permanent_address_field().get_attribute('value') == value
 
     def get_submit_button(self):
         return self.driver.find_element(By.XPATH, '//*[@id="submit"]')
 
-    def submit_button_click(self):
-        self.get_submit_button().click()
+    # def submit_button_click(self):
+    #     self.get_submit_button().click()
 
     def get_output_field(self):
         return self.driver.find_element(By.XPATH, '//*[@id="output"]')
 
-    def output_field_check_values(self, name=None, email=None, current_address=None, permanent_address=None):
-        output_field = self.get_output_field()
-        if name:
-            assert output_field.find_element(By.XPATH, '//*[@id="name"]').text == f'Name:{name}'
-        if email:
-            try:
-                assert output_field.find_element(By.XPATH, '//*[@id="email"]').text == f'Email:{email}'
-            except:
-                assert self.get_email_field().get_attribute('class') == 'mr-sm-2 field-error form-control'
-        if current_address:
-            assert output_field.find_element(By.XPATH, '//p[@id="currentAddress"]').text == f'Current Address :{current_address}'
-        if permanent_address:
-            assert output_field.find_element(By.XPATH,'//p[@id="permanentAddress"]').text == f'Permananet Address :{permanent_address}'
+    # def output_field_check_values(self, name=None, email=None, current_address=None, permanent_address=None):
+    #     output_field = self.get_output_field()
+    #     if name:
+    #         assert output_field.find_element(By.XPATH, '//*[@id="name"]').text == f'Name:{name}'
+    #     if email:
+    #         try:
+    #             assert output_field.find_element(By.XPATH, '//*[@id="email"]').text == f'Email:{email}'
+    #         except:
+    #             assert self.get_email_field().get_attribute('class') == 'mr-sm-2 field-error form-control'
+    #     if current_address:
+    #         assert output_field.find_element(By.XPATH, '//p[@id="currentAddress"]').text == f'Current Address :{current_address}'
+    #     if permanent_address:
+    #         assert output_field.find_element(By.XPATH,'//p[@id="permanentAddress"]').text == f'Permananet Address :{permanent_address}'
