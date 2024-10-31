c295d86cc6827b7b44d6d31442ce5533d009d02a
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-31 18:38:44 +0300

    Убраны методы для самопроверки в классе описывающим ресурс Sortable
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/interaction/sortable.py
+++ pages/interaction/sortable.py
@@ -7,14 +7,14 @@ class Sortable(Driver):
     def get_sortable_list(self):
         return self.driver.find_element(By.XPATH, '//*[@id="demo-tab-list"]')
 
-    def click_sortable_list(self):
-        self.get_sortable_list().click()
+    # def click_sortable_list(self):
+    #     self.get_sortable_list().click()
 
     def get_sortable_grid(self):
         return self.driver.find_element(By.XPATH, '//*[@id="demo-tab-grid"]')
 
-    def click_sortable_grid(self):
-        self.get_sortable_grid().click()
+    # def click_sortable_grid(self):
+    #     self.get_sortable_grid().click()
 
     def get_element_by_text_in_list(self, text):
         return self.get_sortable_list().find_element(By.XPATH, f'//*[contains(text(), "{text}")]')
