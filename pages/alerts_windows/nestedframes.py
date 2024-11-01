c1a625980b0e157abdbb65a79a70ddb1b7f263f5
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-11-01 11:17:18 +0300

    Вынесена проверки из родительского класса в тесты
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- pages/alerts_windows/nestedframes.py
+++ pages/alerts_windows/nestedframes.py
@@ -7,16 +7,5 @@ class NestedFrames(Driver):
     def get_parent_frame(self):
         return self.driver.find_element(By.XPATH, '//*[@id="frame1"]')
 
-    def switch_to_parent_frame(self):
-        frame = self.get_parent_frame()
-        self.driver.switch_to.frame(frame)
-        assert self.driver.find_element(By.XPATH, '//body').text == 'Parent frame'
-
     def get_child_frame(self):
         return self.driver.find_element(By.XPATH, '//*[@srcdoc="<p>Child Iframe</p>"]')
-
-    def switch_to_child_frame(self):
-        self.switch_to_parent_frame()
-        frame = self.get_child_frame()
-        self.driver.switch_to.frame(frame)
-        assert self.driver.find_element(By.XPATH, '//body').text == 'Child Iframe'
