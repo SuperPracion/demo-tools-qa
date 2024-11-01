356c5b1f815fee07dea9ac0e1cdc396f242c7d00
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-11-01 11:24:34 +0300

    Переписаны проверки для ресурса browser page
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_alert_windows/test_browser_windows/test_open_new_tab_window_message.py
+++ tests/test_alert_windows/test_browser_windows/test_open_new_tab_window_message.py
@@ -1,38 +1,40 @@
+import allure
 import pytest
 from selenium.webdriver.common.by import By
 
-from pages.main.main import Main
-from pages.alerts_windows.alerts_windows import AlertsWindows
 from pages.alerts_windows.browser_windows import BrowserWindows
 
 
 @pytest.fixture
-def browser_windows(setup_user, setup_driver):
-    main_page = Main(setup_user, setup_driver)
-    main_page.alerts_windows_button_click()
-    alerts_windows = AlertsWindows(setup_driver)
-    alerts_windows.browser_windows_button_click()
-    browser_windows = BrowserWindows(setup_driver)
-    yield browser_windows
+def browser_page(alerts_windows_page):
+    alerts_windows_page.click(alerts_windows_page.get_browser_windows_button())
+    browser_page = BrowserWindows(alerts_windows_page.driver)
+    yield browser_page
 
 
-def test_open_new_tab(browser_windows):
-    button = browser_windows.get_new_tab_button()
-    browser_windows.click(button)
-    assert len(browser_windows.driver.window_handles) > 1
-    browser_windows.driver.switch_to.window(browser_windows.driver.window_handles[1])
-    assert browser_windows.driver.find_element(By.XPATH, "//*[@id='sampleHeading']").text == 'This is a sample page'
+@allure.title('')
+@allure.step('')
+def test_open_new_tab(browser_page):
+    button = browser_page.get_new_tab_button()
+    browser_page.click(button)
+    assert len(browser_page.driver.window_handles) > 1
+    browser_page.driver.switch_to.window(browser_page.driver.window_handles[1])
+    assert browser_page.driver.find_element(By.XPATH, "//*[@id='sampleHeading']").text == 'This is a sample page'
 
 
-def test_open_new_window(browser_windows):
-    button = browser_windows.get_new_window_button()
-    browser_windows.click(button)
-    assert len(browser_windows.driver.window_handles) > 1
-    browser_windows.driver.switch_to.window(browser_windows.driver.window_handles[1])
-    assert browser_windows.driver.find_element(By.XPATH, "//*[@id='sampleHeading']").text == 'This is a sample page'
+@allure.title('')
+@allure.step('')
+def test_open_new_window(browser_page):
+    button = browser_page.get_new_window_button()
+    browser_page.click(button)
+    assert len(browser_page.driver.window_handles) > 1
+    browser_page.driver.switch_to.window(browser_page.driver.window_handles[1])
+    assert browser_page.driver.find_element(By.XPATH, "//*[@id='sampleHeading']").text == 'This is a sample page'
 
 
-def test_open_new_windows_message(browser_windows):
-    button = browser_windows.get_new_window_message_button()
-    browser_windows.click(button)
-    assert len(browser_windows.driver.window_handles) > 1
+@allure.title('')
+@allure.step('')
+def test_open_new_windows_message(browser_page):
+    button = browser_page.get_new_window_message_button()
+    browser_page.click(button)
+    assert len(browser_page.driver.window_handles) > 1
