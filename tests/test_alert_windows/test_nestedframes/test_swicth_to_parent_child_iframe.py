46462405fba861fa3925e856cc36d4da63e0508e
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-11-01 11:25:06 +0300

    Переписаны проверки для ресурса nestedframes
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_alert_windows/test_nestedframes/test_swicth_to_parent_child_iframe.py
+++ tests/test_alert_windows/test_nestedframes/test_swicth_to_parent_child_iframe.py
@@ -1,23 +1,37 @@
-import pytest
+from selenium.webdriver.common.by import By
 
+from tests.conftest import *
 from pages.main.main import Main
 from pages.alerts_windows.alerts_windows import AlertsWindows
 from pages.alerts_windows.nestedframes import NestedFrames
 
 
-@pytest.fixture
-def nestedframes(setup_user, setup_driver):
+@allure.title('')
+def test_switch_to_parent_frame(setup_user, setup_driver):
     main_page = Main(setup_user, setup_driver)
-    main_page.alerts_windows_button_click()
-    alerts_windows = AlertsWindows(setup_driver)
+    main_page.click(main_page.get_alerts_windows_button())
+    alerts_windows = AlertsWindows(main_page.driver)
     alerts_windows.nested_frames_click()
-    nestedframes = NestedFrames(setup_driver)
-    yield nestedframes
+
+    nestedframes = NestedFrames(alerts_windows.driver)
+    frame = nestedframes.get_parent_frame()
+    nestedframes.driver.switch_to.frame(frame)
+
+    assert nestedframes.driver.find_element(By.XPATH, '//body').text == 'Parent frame'
 
 
-def test_switch_to_parent_frame(nestedframes):
-    nestedframes.switch_to_parent_frame()
+@allure.title('')
+def test_switch_to_child_frame(setup_user, setup_driver):
+    main_page = Main(setup_user, setup_driver)
+    main_page.click(main_page.get_alerts_windows_button())
+    alerts_windows = AlertsWindows(main_page.driver)
+    alerts_windows.nested_frames_click()
 
+    nestedframes = NestedFrames(alerts_windows.driver)
+    parent_frame = nestedframes.get_parent_frame()
+    nestedframes.driver.switch_to.frame(parent_frame)
 
-def test_switch_to_child_frame(nestedframes):
-    nestedframes.switch_to_child_frame()
+    child_frame = nestedframes.get_child_frame()
+    nestedframes.driver.switch_to.frame(child_frame)
+
+    assert nestedframes.driver.find_element(By.XPATH, '//body').text == 'Child Iframe'
