3270489dfdaca25eb585090c1f6a975857278495
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-11-01 11:23:53 +0300

    Переписаны проверки для ресурса Modal
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_alert_windows/test_modal_dialogs/test_open_modal.py
+++ tests/test_alert_windows/test_modal_dialogs/test_open_modal.py
@@ -1,43 +1,52 @@
+import allure
 import pytest
 
-from pages.main.main import Main
-from pages.alerts_windows.alerts_windows import AlertsWindows
 from pages.alerts_windows.modal_dialogs import ModalDialogs
 
 
+@allure.step('open modal dialogs page')
 @pytest.fixture
-def modal_dialogs(setup_user, setup_driver):
-    main_page = Main(setup_user, setup_driver)
-    main_page.alerts_windows_button_click()
-    alerts_windows = AlertsWindows(setup_driver)
-    alerts_windows.modal_dialogs_button_click()
-    modal_dialogs = ModalDialogs(setup_driver)
-    yield modal_dialogs
+def modal_dialogs_page(alerts_windows_page):
+    alerts_windows_page.click(alerts_windows_page.get_modal_dialogs_button())
+    modal_dialogs_page = ModalDialogs(alerts_windows_page.driver)
+    yield modal_dialogs_page
 
 
-def test_open_small_modal(modal_dialogs):
-    modal_dialogs.small_modal_button_click()
+@allure.title('')
+@allure.step('')
+def test_open_small_modal(modal_dialogs_page):
+    modal_dialogs_page.small_modal_button_click()
 
 
-def test_open_lange_modal(modal_dialogs):
-    modal_dialogs.large_modal_button_click()
+@allure.title('')
+@allure.step('')
+def test_open_lange_modal(modal_dialogs_page):
+    modal_dialogs_page.large_modal_button_click()
 
 
-def test_small_dialog_close(modal_dialogs):
-    modal_dialogs.small_modal_button_click()
-    modal_dialogs.close_button_click()
+@allure.title('')
+@allure.step('')
+def test_small_dialog_close(modal_dialogs_page):
+    modal_dialogs_page.small_modal_button_click()
+    modal_dialogs_page.close_button_click()
 
 
-def test_large_dialog_close(modal_dialogs):
-    modal_dialogs.large_modal_button_click()
-    modal_dialogs.close_button_click()
+@allure.title('')
+@allure.step('')
+def test_large_dialog_close(modal_dialogs_page):
+    modal_dialogs_page.large_modal_button_click()
+    modal_dialogs_page.close_button_click()
 
 
-def test_small_dialog_close_with_x(modal_dialogs):
-    modal_dialogs.small_modal_button_click()
-    modal_dialogs.small_button_close_click()
+@allure.title('')
+@allure.step('')
+def test_small_dialog_close_with_x(modal_dialogs_page):
+    modal_dialogs_page.small_modal_button_click()
+    modal_dialogs_page.small_button_close_click()
 
 
-def test_large_dialog_close_with_x(modal_dialogs):
-    modal_dialogs.large_modal_button_click()
-    modal_dialogs.small_button_close_click()
+@allure.title('')
+@allure.step('')
+def test_large_dialog_close_with_x(modal_dialogs_page):
+    modal_dialogs_page.large_modal_button_click()
+    modal_dialogs_page.small_button_close_click()
