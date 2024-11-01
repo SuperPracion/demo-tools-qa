c5f3bee5282b731f83816460fc19761bdd57dc4e
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-11-01 16:11:25 +0300

    Добавлено формирование скриншотов в ходе выполнения тестов
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- tests/test_alert_windows/test_alers/test_accept_alerts.py
+++ tests/test_alert_windows/test_alers/test_accept_alerts.py
@@ -1,6 +1,7 @@
 import time
 import pytest
 import allure
+from allure_commons.types import AttachmentType
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
@@ -8,6 +9,7 @@ from pages.alerts_windows.alerts import Alerts
 from base.element_found_exception import ElementFoundException
 
 
+@allure.step('open alerts page')
 @pytest.fixture
 def alerts_page(alerts_windows_page):
     alerts_windows_page.click(alerts_windows_page.get_alerts_button())
@@ -15,7 +17,7 @@ def alerts_page(alerts_windows_page):
     yield alerts_page
 
 
-@allure.title('Accept simple alert with check text')
+@allure.title('Test accept simple alert with check text')
 class TestSimpleAlert:
     @allure.step('check text in simple alert')
     def test_accept_simple_alert(self, alerts_page):
@@ -28,9 +30,10 @@ class TestSimpleAlert:
             raise ElementFoundException
         except TimeoutException:
             pass
+        allure.attach(alerts_page.driver.get_screenshot_as_png(), 'accept_simple_alert', AttachmentType.PNG)
 
 
-@allure.title('Accept timer alert')
+@allure.title('Test accept timer alert')
 class TestTimerAlert:
     @allure.step('accept alert after timer')
     def test_accept_timer_alert(self, alerts_page):
@@ -44,9 +47,10 @@ class TestTimerAlert:
             raise ElementFoundException
         except TimeoutException:
             pass
+        allure.attach(alerts_page.driver.get_screenshot_as_png(), 'accept_timer_alert', AttachmentType.PNG)
 
 
-@allure.title('Check alert witch confirm')
+@allure.title('Test check alert witch confirm')
 class TestConfirmAlert:
     @allure.step('accept confirm alert')
     def test_accept_confirm_alert(self, alerts_page):
@@ -55,6 +59,7 @@ class TestConfirmAlert:
         assert alerts_page.get_alert().text == 'Do you confirm action?'
         alerts_page.accept_alert()
         assert alerts_page.driver.find_element(By.XPATH, '//*[@id="confirmResult"]').text == 'You selected Ok'
+        allure.attach(alerts_page.driver.get_screenshot_as_png(), 'accept_confirm_alert', AttachmentType.PNG)
 
     @allure.step('dismiss confirm alert')
     def test_cancel_confirm_alert(self, alerts_page):
@@ -63,9 +68,10 @@ class TestConfirmAlert:
         assert alerts_page.get_alert().text == 'Do you confirm action?'
         alerts_page.dismiss_alert()
         assert alerts_page.driver.find_element(By.XPATH, '//*[@id="confirmResult"]').text == 'You selected Cancel'
+        allure.attach(alerts_page.driver.get_screenshot_as_png(), 'cancel_confirm_alert', AttachmentType.PNG)
 
 
-@allure.title('Alert with promt')
+@allure.title('Test alert with promt')
 class TestPromtAlert:
     @allure.step('accept promt alert')
     def test_accept_promt_alert(sefl, alerts_page):
@@ -75,6 +81,7 @@ class TestPromtAlert:
         alerts_page.get_alert().send_keys('123')
         alerts_page.accept_alert()
         assert alerts_page.driver.find_element(By.XPATH, '//*[@id="promptResult"]').text == f'You entered 123'
+        allure.attach(alerts_page.driver.get_screenshot_as_png(), 'accept_promt_alert', AttachmentType.PNG)
 
     @allure.step('dismiss promt alert')
     def test_cancel_promt_alert(self, alerts_page):
@@ -87,3 +94,4 @@ class TestPromtAlert:
             raise ElementFoundException
         except TimeoutException:
             pass
+        allure.attach(alerts_page.driver.get_screenshot_as_png(), 'cancel_promt_alert', AttachmentType.PNG)
